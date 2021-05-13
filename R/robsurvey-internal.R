de06d8339898e45a27ac345e49c90749b88c396e
Tobias Schoch <tobias.schoch@gmail.com>
2021-05-13 20:27:32 +0200

    cleaned Rd-files

--- R/robsurvey-internal.R
+++ R/robsurvey-internal.R
@@ -167,6 +167,6 @@
      88   Y8. .8P 88  dP  \\__ \\ |_| | |   \\ V /  __/ |_| |
      88    'Y8P'  88e8P'  |___/\\__,_|_|    \\_/ \\___|\\__, |
 						     __/ |
-					version 0.2 |___/\n")
+					version 0.2 |___/\n\ntype: package?robsurvey to learn more\n")
    }
 }
