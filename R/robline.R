a058e8ad249cbc65c545aee2f0e6b1ee227f8131
Tobias Schoch <tobias.schoch@gmail.com>
2022-04-16 16:39:43 +0200

    improved medline functions + added S3 utility functions

--- R/robline.R
+++ R/robline.R
@@ -1,18 +1,34 @@
+# quantiles as implemented in line() but with weights x and w sorted
+# according to x
+.line_quantile <- function(x, w, prob)
+{
+    n <- length(x)
+    cumw <- n * cumsum(w) / sum(w)
+    low <- min(which(cumw >= (n - 1) * prob))
+    high <- max(which(cumw <= (n - 1) * prob))
+    return((x[low + 1] + x[high + 1]) /2)
+}
+# check data for missing values
+.check_line_data <- function(x, y, w, na.rm = FALSE)
+{
+    ok <- stats::complete.cases(x, y, w)
+    if (sum(ok) < length(x)) {
+        if (na.rm) {
+	        x <- x[ok]
+            y <- y[ok]
+            w <- w[ok]
+        } else {
+	        stop("There are missing values in 'x', 'y' or 'w'.\n",
+                call. = FALSE)
+        }
+    }
+    list(x = x, y = y, w = w)
+}
 # Weighted line estimator
 weighted_line <- function(x, y = NULL, w, na.rm = FALSE, iter = 1)
 {
-    if(missing(w)) stop("Argument 'w' (weights) is missing, with no default.\n")
-
-    # quantiles as implemented in line() but with weights x and w sorted
-    # according to x
-    line.quantile <- function(x, w, prob)
-    {
-        n <- length(x)
-        cumw <- n * cumsum(w) / sum(w)
-        low <- min(which(cumw >= (n - 1) * prob))
-        high <- max(which(cumw <= (n - 1) * prob))
-        return((x[low + 1] + x[high + 1]) /2)
-    }
+    if(missing(w))
+        stop("Argument 'w' (weights) is missing, with no default.\n")
 
     if (inherits(x, "formula")) {
         mf <- stats::model.frame(x)
@@ -23,18 +39,9 @@ weighted_line <- function(x, y = NULL, w, na.rm = FALSE, iter = 1)
         stop("Argument 'x' contains more than 1 explanatory variables.\n",
             call. = FALSE)
 
-#FIXME: this block is used in several function => separate function
-    dat <- data.frame(x, y, w)
-    ok <- stats::complete.cases(dat$x, dat$y, dat$w)
-    n <- sum(ok)
-    if (n < length(x)) {
-        if (na.rm) {
-	        x <- dat$x[ok]; y <- dat$y[ok]; w <- dat$w[ok]
-        } else {
-	        stop("There are missing values in 'x', 'y' or 'w'.\n",
-                call. = FALSE)
-        }
-    }
+    # check for missing values
+    tmp <- .check_line_data(x, y, w, na.rm)
+    x <- tmp$x; y <- tmp$y; w <- tmp$w; n <- length(x)
 
     # Sort data according to x
     ord <- order(x)
@@ -45,8 +52,8 @@ weighted_line <- function(x, y = NULL, w, na.rm = FALSE, iter = 1)
     w <- n * w[ord] / sum(w)
 
     # Groups
-    lim1 <- line.quantile(x, w, 1 / 3)
-    lim2 <- line.quantile(x, w, 2 / 3)
+    lim1 <- .line_quantile(x, w, 1 / 3)
+    lim2 <- .line_quantile(x, w, 2 / 3)
     groups <- (x <= lim1) * 1 + (x > lim1 & x < lim2) * 2 + (x >= lim2) * 3
 
     # Medians for x
@@ -61,7 +68,7 @@ weighted_line <- function(x, y = NULL, w, na.rm = FALSE, iter = 1)
         slope <- slope + (wmedr[2] - wmedr[1]) / (wmedx[2] - wmedx[1])
         r <- y - slope * x
         j <- j + 1
-   }
+    }
 
     # intercept and predicted values
     intercept <- weighted_median(r, w, na.rm)
@@ -87,34 +94,24 @@ weighted_median_line <- function(x, y = NULL, w, type = "slopes",
         stop("Argument 'x' contains more than 1 explanatory variables.\n",
             call. = FALSE)
 
-    # Robustification type
-    stype <- pmatch(type, c("slopes"), nomatch = 2)
-
-    # Ensure case-wise completeness
-    ok <- stats::complete.cases(data.frame(x, y, w))
-    n <- sum(ok)
-    if (n < length(x)) {
-        if (na.rm) {
-	        x <- x[ok]; y <- y[ok]; w <- w[ok]
-        } else {
-	        stop("There are missing values in 'x', 'y' or 'w'.\n",
-                call. = FALSE)
-        }
-    }
+    # check for missing values
+    tmp <- .check_line_data(x, y, w, na.rm)
+    x <- tmp$x; y <- tmp$y; w <- tmp$w
 
     # univariate medians
     wmedx <- weighted_median(x, w, na.rm)
     wmedy <- weighted_median(y, w, na.rm)
 
-    # slope
-    slope <- switch(stype,
-        {
+    # type (match.arg is used for partial matching)
+    slope <- switch(match.arg(type, c("slopes", "products")),
+        "slopes" = {
             tmp <- (y - wmedy) / (x - wmedx)
             at <- is.finite(tmp)
             weighted_median(tmp[at], w[at], na.rm = TRUE)
         },
-        weighted_median((y - wmedy) * (x - wmedx), w, na.rm = TRUE) /
-	        weighted_median((x - wmedx)^2, w, na.rm = TRUE)
+        "products" = weighted_median((y - wmedy) * (x - wmedx), w,
+            na.rm = TRUE) / weighted_median((x - wmedx)^2, w, na.rm = TRUE),
+        stop("Argument 'type' must be either 'slopes' or 'products'\n")
    )
 
     # residuals
@@ -143,17 +140,9 @@ weighted_median_ratio <- function(x, y = NULL, w, na.rm = FALSE)
         stop("Argument 'x' contains more than 1 explanatory variables.\n",
             call. = FALSE)
 
-    # Ensure case-wise completeness
-    ok <- stats::complete.cases(data.frame(x, y, w))
-    n <- sum(ok)
-    if (n < length(x)) {
-        if (na.rm) {
-	        x <- x[ok]; y <- y[ok]; w <- w[ok]
-        } else {
-	        stop("There are missing values in 'x', 'y' or 'w'.\n",
-                call. = FALSE)
-        }
-    }
+    # check for missing values
+    tmp <- .check_line_data(x, y, w, na.rm)
+    x <- tmp$x; y <- tmp$y; w <- tmp$w
 
     # ratio
     ratio <- weighted_median(y / x, w, na.rm)
@@ -164,4 +153,24 @@ weighted_median_ratio <- function(x, y = NULL, w, na.rm = FALSE)
 
     structure(list(call = match.call(), coefficients = ratio,
         residuals = y - yhat, fitted.values = yhat), class = "medline")
+}
+# some S3 functions for objects of class 'medline'
+print.medline <- function(x, ...)
+{
+    cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"),
+        "\n", sep = "")
+    cat("\nCoefficients:\n")
+    print(x$coefficients)
+}
+coef.medline <- function(object, ...)
+{
+    object$coefficients
+}
+residuals.medline <- function(object, ...)
+{
+    object$residuals
+}
+fitted.medline <- function(object, ...)
+{
+    object$fitted.values
 }
