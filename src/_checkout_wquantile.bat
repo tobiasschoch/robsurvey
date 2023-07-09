67cdc56fac7908a12f63bfe832f206868e3de547
Tobias Schoch <tobias.schoch@gmail.com>
2023-07-09 19:06:20 +0200

    Trash `Makefile` in src folder

--- src/_checkout_wquantile.bat
+++
@@ -1,4 +0,0 @@
-REM checkout wquantile.c 
-PowerShell.exe wget -Uri "https://raw.githubusercontent.com/tobiasschoch/wquantile/master/src/wquantile.c" -outfile "wquantile.c"
-REM checkout wquantile.h 
-PowerShell.exe wget -Uri "https://raw.githubusercontent.com/tobiasschoch/wquantile/master/src/wquantile.h" -outfile "wquantile.h" 
